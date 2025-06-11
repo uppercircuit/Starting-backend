4a039abe03be812ae971a55510837c66a618941d
uppercircuit <adityashrikul0227@gmail.com>
2025-06-11 11:51:23 +0530

    add regsiter controller

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+import express from "express"
 import dotenv from "dotenv"
+
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+//const app= express();
+//app()
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000, () =>{
+    app.listen(process.env.PORT || 3000, () =>{
        console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose,{Schema} from mongoose;
+import mongoose,{Schema} from 'mongoose';
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
@@ -53,8 +53,8 @@ const userSchema=new Schema(
 )
 
 userSchema.pre("save",async function name(next) {
-    if(this.isModified("password")) return next();
-    this.password=bcrypt.hash(this.password,10)
+    if(!this.isModified("password")) return next();
+    this.password=await bcrypt.hash(this.password,10)
     next()
 }
 )
